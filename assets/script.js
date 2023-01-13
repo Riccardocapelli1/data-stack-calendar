cada695dfaf991d82e08001a19e50c1868537b5a
Riccardocapelli1 <riccardocapellimi91@gmail.com>
2023-01-14 00:29:12 +0100

    third commit

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,17 @@
-function changeBackground() {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
- }
+// Seleziona tutti i titoli dei paragrafi
+const paragraphs = document.querySelectorAll("h2");
+
+// Itera attraverso tutti i titoli dei paragrafi
+for (let i = 0; i < paragraphs.length; i++) {
+  // Assegna un evento al passaggio del mouse
+  paragraphs[i].addEventListener("mouseover", function() {
+    // Ingrandisci il titolo del paragrafo
+    this.style.fontSize = "30px";
+  });
+
+  // Assegna un evento all'uscita del mouse
+  paragraphs[i].addEventListener("mouseout", function() {
+    // Ripristina le dimensioni originali del titolo del paragrafo
+    this.style.fontSize = "25px";
+  });
+}
