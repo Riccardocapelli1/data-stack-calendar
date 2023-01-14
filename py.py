57ae280cb02a851d2fce0c4338e26311d73b2495
Riccardocapelli1 <riccardocapellimi91@gmail.com>
2023-01-14 01:48:24 +0100

    change to data type in py.py

--- py.py
+++ py.py
@@ -5,10 +5,10 @@ import os
 
 # Accedi alla secret chiamata "" utilizzando il modulo `parser`
 parser = argparse.ArgumentParser()
-consumer_key = parser.add_argument("consumer_key", help="consumer_key")
-consumer_secret = parser.add_argument("consumer_secret", help="consumer_secret")
-access_token = parser.add_argument("access_token", help="access_token")
-access_token_secret = parser.add_argument("access_token_secret", help="access_token_secret")
+consumer_key = parser.add_argument("consumer_key", type=str, help="consumer_key")
+consumer_secret = parser.add_argument("consumer_secret", type=str, help="consumer_secret")
+access_token = parser.add_argument("access_token", type=str, help="access_token")
+access_token_secret = parser.add_argument("access_token_secret", type=str, help="access_token_secret")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
