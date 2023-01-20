9f8988d71e03d185872ac7c221299ab60b62e83c
Riccardo Capelli <93529394+Riccardocapelli1@users.noreply.github.com>
2023-01-20 23:42:53 +0100

    Update py.py
    Working version py.py with user name on each tweet

--- py.py
+++ py.py
@@ -88,7 +88,7 @@ for _, row in df.iterrows():
     current_user = user
     
     # Crea un'ancora per ogni utente nell'HTML
-html_content += f"  <h2 class='h2' style='font-weight: bold; text-transform: uppercase; margin: 2em 0;' id='user{users_dict[user]}'>{user}</h2>\n"        
+    html_content += f"  <h2 class='h2' style='font-weight: bold; text-transform: uppercase; margin: 2em 0;' id='user{users_dict[user]}'>{user}</h2>\n"        
     date = row["Time"]
     text = make_link(row["Tweet"])
     html_content += f" <h3 date='{date}'>{date}</h3>\n"
