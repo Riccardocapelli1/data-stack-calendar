690059a11ec5c60058d4d5f887f6e7b181e91bc8
Riccardocapelli1 <riccardocapellimi91@gmail.com>
2023-01-26 18:23:44 +0100

    py.py

--- py.py
+++ py.py
@@ -14,7 +14,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 profiles = ["AirbyteHQ","ApacheAirflow","ApacheArrow","ApacheCalcite","ApacheFlink","apachekafka","apachenifi","ApacheParquet","ApachePinot","apachesuperset","awscloud","Azure","Azure_Synapse","ClickHouseDB","code","confluentinc","dask_dev","dagster","dbt_labs","DeepMind","Docker","druidio","duckdb","elastic","expectgreatdata","fastdotai","getdbt","github","gitlab","googlecloud","grafana","ksqlDB","kubernetesio","lightdash_devs","mariadb","Materialize","meltanodata","Metabase","MySQL","motherduck","montecarlodata","MSPowerBI","numpy_team","pandas_dev","PyData","PostgreSQL","ProjectJupyter","PrefectIO","ScyllaDB","singer_io","SnowflakeDB","SQLServer","tableau","thecubejs","thoughtspot"]
 
 #df2 definire le keyword da cercare
-keywords = ["conference", "event", "podcast", "course", "training", "badge", "*"]
+keywords = ["conference", "event", "podcast", "course", "training", "badge"," "]
 
 # Crea una lista vuota per i tweet
 tweets = []
