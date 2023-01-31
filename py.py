f34ce8b9b043a414b050bad0ce92ff7db2325899
Riccardocapelli1 <riccardocapellimi91@gmail.com>
2023-01-31 23:24:54 +0100

    separate pages for conference, podcast and courses

--- py.py
+++ py.py
@@ -54,8 +54,21 @@ df['Time'] = pd.to_datetime(df['Time'], format='%Y-%m-%d %H:%M:%S').apply(lambda
 df["User"] = df["User"].str.upper()
 
 # Filtra il dataframe per i tweet che contengono le parole "event" o "conference" nel testo
-df = df[df['Tweet'].str.contains('Event|event|Conference|conference|Podcast|podcast|Badge|badge|Certific|certific|Webinar|webinar|free resources|free courses|free learning')]
+df_ = df[df['Tweet'].str.contains('Event|event|Conference|conference|Podcast|podcast|Badge|badge|Certific|certific|Webinar|webinar|free resources|free courses|free learning')]
 df = df[~df['Tweet'].str.contains('Of courses|of courses|event log|Event log|Steven|steven|Prevent|prevent|Event streaming|event streaming|SSL certificate|GhEvent|EventTimer')]
+
+# Filtra il dataframe per i tweet che contengono le parole "event" o "conference" nel testo
+df_conference = df[df['Tweet'].str.contains('Event|event|Conference|conference|Webinar|webinar')]
+df_conference = df[~df['Tweet'].str.contains('event log|Event log|Steven|steven|Prevent|prevent|Event streaming|event streaming|SSL certificate|GhEvent|EventTimer')]
+
+# Filtra il dataframe per i tweet che contengono le parole "certificate" o "courses" nel testo
+df_certification = df[df['Tweet'].str.contains('Badge|badge|Certific|certific|free resources|free courses|free learning')]
+df_certification = df[~df['Tweet'].str.contains('Of courses|of courses|SSL certificate')]
+
+# Filtra il dataframe per i tweet che contengono le parole "event" o "conference" nel testo
+df_podcast = df[df['Tweet'].str.contains('Podcast|podcast')]
+#df_podcast = df[~df['Tweet'].str.contains('')]
+
 
 # creare una copia del dataframe
 df2 = df.copy()
@@ -111,6 +124,7 @@ chart_data = chart_data[:-2]
 googleapi = "https://fonts.googleapis.com/css?family=Inconsolata|Roboto"
 googleapi = googleapi.replace(googleapi,f'<link href="{googleapi}" rel="stylesheet" >')
 
+###
 # crea contenuto html principale
 def make_link(text):
     # Cerca tutte le occorrenze di link nella stringa
@@ -136,6 +150,26 @@ html_content += "  <meta name='viewport' content='width=device-width, initial-sc
 html_content += "  <script src='assets/script.js'></script>\n"
 html_content += "  <script src='assets/load.js'></script>\n"
 html_content += "  <title>Hacked-data-stack intel for the data and analytics communities</title>\n"
+
+html_content += "    <header class='header-area header-sticky'>\n"
+html_content += "        <div class='container'>\n"
+html_content += "            <div class='row'>\n"
+html_content += "                <div class='col-12'>\n"
+html_content += "                    <nav class='main-nav'>\n"
+html_content += "                        <ul class='nav'>\n"
+html_content += "                            <li><a href='index.html' class='active'>Conferences</a></li>\n"
+html_content += "                            <li><a href='certificate.html'>Certificates</a></li>\n"
+html_content += "                            <li><a href='podcast.html'>Podcasts</a></li>\n"
+html_content += "                        </ul>        \n"
+html_content += "                        <a class='menu-trigger'>\n"
+html_content += "                            <span>Menu</span>\n"
+html_content += "                        </a>\n"
+html_content += "                        <!-- ***** Menu End ***** -->\n"
+html_content += "                    </nav>\n"
+html_content += "                </div>\n"
+html_content += "            </div>\n"
+html_content += "        </div>\n"
+html_content += "    </header>\n"
 
 # Titolo all'html_content
 html_content += "  <h1>Events, conferences, podcast and training list up-to-date</h1>\n"
@@ -186,7 +220,7 @@ users_dict = {}
 
 # Utilizza un ciclo for per aggiungere gli utenti al dizionario e tenere traccia della loro posizione nell'HTML
 pos = 0
-for _, row in df.iterrows():
+for _, row in df_certification.iterrows():
     user = row["Profile"]
     if user not in users_dict:
         users_dict[user] = pos
@@ -200,10 +234,10 @@ for user in users_dict:
 html_content += "  </ul>\n"
 
 # Utilizza un ciclo for per iterare attraverso ogni riga del dataframe
-current_user = df.iloc[0]["Profile"]
+current_user = df_certification.iloc[0]["Profile"]
 html_content += f"  <h2 class='h2' style='text-transform: uppercase;' id='user{users_dict[current_user]}'>{current_user} </h2><br><a href='{row['Profile_web']}'>Official website</a><br><a href='{row['Profile_twi']}'>Twitter profile</a>\n"
 
-for _, row in df.iterrows():
+for _, row in df_certification.iterrows():
     user = row["Profile"]
     if user != current_user:
         current_user = user
@@ -223,6 +257,278 @@ html_content += "</html>\n"
 
 with open("./index.html", "w") as f:
     f.write(html_content)
+###
+
+
+###
+# crea contenuto html principale
+def make_link(text):
+    # Cerca tutte le occorrenze di link nella stringa
+    links = re.findall(r'(https?:\/\/\S+)', text)
+    # Sostituisci ogni occorrenza di link con il link cliccabile
+    for link in links:
+        text = text.replace(link, f'<a href="{link}">{link}</a>')
+    return text
+
+# Crea una stringa vuota per il contenuto del file HTML
+html_content = ""
+
+# Aggiungi il contenuto iniziale del file HTML
+html_content  = "<!DOCTYPE html>\n"
+html_content += "<html>\n"
+html_content += "<head>\n"
+html_content += googleapi + "\n"
+html_content += "  " + countapi + "\n"
+html_content += "  <meta charset='utf-8'>\n"
+html_content += "  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=yes'>\n"
+html_content += "  <link rel='stylesheet' type='text/css' href='assets/style.css'>\n"
+html_content += "  <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
+html_content += "  <script src='assets/script.js'></script>\n"
+html_content += "  <script src='assets/load.js'></script>\n"
+html_content += "  <title>Hacked-data-stack intel for the data and analytics communities</title>\n"
+
+html_content += "    <header class='header-area header-sticky'>\n"
+html_content += "        <div class='container'>\n"
+html_content += "            <div class='row'>\n"
+html_content += "                <div class='col-12'>\n"
+html_content += "                    <nav class='main-nav'>\n"
+html_content += "                        <ul class='nav'>\n"
+html_content += "                            <li><a href='index.html'>Conferences</a></li>\n"
+html_content += "                            <li><a href='certificate.html' class='active'>Certificates</a></li>\n"
+html_content += "                            <li><a href='podcast.html'>Podcasts</a></li>\n"
+html_content += "                        </ul>        \n"
+html_content += "                        <a class='menu-trigger'>\n"
+html_content += "                            <span>Menu</span>\n"
+html_content += "                        </a>\n"
+html_content += "                        <!-- ***** Menu End ***** -->\n"
+html_content += "                    </nav>\n"
+html_content += "                </div>\n"
+html_content += "            </div>\n"
+html_content += "        </div>\n"
+html_content += "    </header>\n"
+
+# Titolo all'html_content
+html_content += "  <h1>Events, conferences, podcast and training list up-to-date</h1>\n"
+html_content += "  <p>A tweet aggregator site dysplaying events, conferences, podcast, certification and courses links that can be useful for data practitioners of all kinds. Hope you enjoy ;) </p>\n"
+
+# Aggiungi il codice per il grafico all'html_content
+html_content += "<div class='container'>\n"
+html_content += "<canvas id='myChart'></canvas>\n"
+html_content += "</div>\n"
+html_content += "  <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>\n"
+html_content += "<script>\n"
+html_content += "var ctx = document.getElementById('myChart').getContext('2d');\n"
+html_content += "var myChart = new Chart(ctx, {\n"
+html_content += "  type: 'line',\n"
+html_content += "  data: {\n"
+html_content += "    datasets: [\n"
+html_content += chart_data + "\n"
+html_content += "    ]\n"
+html_content += "  },\n"
+html_content += "  options: {\n"
+html_content += "    scales: {\n"
+html_content += "      xAxes: [{\n"
+html_content += "        type: 'time',\n"
+html_content += "        time: {\n"
+html_content += "           unit: 'month',\n"
+html_content += "           displayFormats: {\n"
+html_content += "            month: 'YYYY-MM'\n"
+html_content += "           }\n"
+html_content += "        }\n"
+html_content += "      }],\n"
+html_content += "      yAxes: [{\n"
+html_content += "        ticks: {\n"
+html_content += "          beginAtZero: true\n"
+html_content += "        }\n"
+html_content += "      }]\n"
+html_content += "    }\n"
+html_content += "  }\n"
+html_content += "});\n"
+html_content += "</script>\n"
+html_content += "  <p>Aggregated kewords tweeted stats by month</p>\n"
+
+
+html_content += "</head>\n"
+html_content += "<body>\n"
+
+#, Crea un dizionario vuoto per gli utenti
+users_dict = {}
+
+# Utilizza un ciclo for per aggiungere gli utenti al dizionario e tenere traccia della loro posizione nell'HTML
+pos = 0
+for _, row in df_certification.iterrows():
+    user = row["Profile"]
+    if user not in users_dict:
+        users_dict[user] = pos
+        pos += 1
+
+# Utilizza un ciclo for per creare un elenco di link agli utenti nell'HTML
+html_content += "  <h3>List of Authors</h3>\n"
+html_content += "  <ul>\n"
+for user in users_dict:
+    html_content += f"    <li><a href='#user{users_dict[user]}'>{user}</a></li>\n"
+html_content += "  </ul>\n"
+
+# Utilizza un ciclo for per iterare attraverso ogni riga del dataframe
+current_user = df_certification.iloc[0]["Profile"]
+html_content += f"  <h2 class='h2' style='text-transform: uppercase;' id='user{users_dict[current_user]}'>{current_user} </h2><br><a href='{row['Profile_web']}'>Official website</a><br><a href='{row['Profile_twi']}'>Twitter profile</a>\n"
+
+for _, row in df_certification.iterrows():
+    user = row["Profile"]
+    if user != current_user:
+        current_user = user
+        
+# Crea un'ancora per ogni utente nell'HTML
+        html_content += f"  <h2 class='h2' style='text-transform: uppercase;' id='user{users_dict[user]}'>{user}</h2><br><a href='{row['Profile_web']}'>Official website</a><br><a href='{row['Profile_twi']}'>Twitter profile</a>\n"
+    date = row["Time"]
+    text = make_link(row["Tweet"])
+    html_content += f" <h3 date='{date}'>{date}</h3>\n"
+    html_content += f"  <p>{text}</p>\n"
+
+html_content += "</body>\n"
+html_content += "<footer>\n"
+html_content += "  <p>Made with ❤️ for the community by Riccardo</p>\n"
+html_content += "</footer>\n"
+html_content += "</html>\n"
+
+with open("./certificate.html", "w") as f:
+    f.write(html_content)
+###
+
+###
+# crea contenuto html principale
+def make_link(text):
+    # Cerca tutte le occorrenze di link nella stringa
+    links = re.findall(r'(https?:\/\/\S+)', text)
+    # Sostituisci ogni occorrenza di link con il link cliccabile
+    for link in links:
+        text = text.replace(link, f'<a href="{link}">{link}</a>')
+    return text
+
+# Crea una stringa vuota per il contenuto del file HTML
+html_content = ""
+
+# Aggiungi il contenuto iniziale del file HTML
+html_content  = "<!DOCTYPE html>\n"
+html_content += "<html>\n"
+html_content += "<head>\n"
+html_content += googleapi + "\n"
+html_content += "  " + countapi + "\n"
+html_content += "  <meta charset='utf-8'>\n"
+html_content += "  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=yes'>\n"
+html_content += "  <link rel='stylesheet' type='text/css' href='assets/style.css'>\n"
+html_content += "  <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
+html_content += "  <script src='assets/script.js'></script>\n"
+html_content += "  <script src='assets/load.js'></script>\n"
+html_content += "  <title>Hacked-data-stack intel for the data and analytics communities</title>\n"
+
+html_content += "    <header class='header-area header-sticky'>\n"
+html_content += "        <div class='container'>\n"
+html_content += "            <div class='row'>\n"
+html_content += "                <div class='col-12'>\n"
+html_content += "                    <nav class='main-nav'>\n"
+html_content += "                        <ul class='nav'>\n"
+html_content += "                            <li><a href='index.html'>Conferences</a></li>\n"
+html_content += "                            <li><a href='certificate.html'>Certificates</a></li>\n"
+html_content += "                            <li><a href='podcast.html' class='active'>Podcasts</a></li>\n"
+html_content += "                        </ul>        \n"
+html_content += "                        <a class='menu-trigger'>\n"
+html_content += "                            <span>Menu</span>\n"
+html_content += "                        </a>\n"
+html_content += "                        <!-- ***** Menu End ***** -->\n"
+html_content += "                    </nav>\n"
+html_content += "                </div>\n"
+html_content += "            </div>\n"
+html_content += "        </div>\n"
+html_content += "    </header>\n"
+
+# Titolo all'html_content
+html_content += "  <h1>Events, conferences, podcast and training list up-to-date</h1>\n"
+html_content += "  <p>A tweet aggregator site dysplaying events, conferences, podcast, certification and courses links that can be useful for data practitioners of all kinds. Hope you enjoy ;) </p>\n"
+
+# Aggiungi il codice per il grafico all'html_content
+html_content += "<div class='container'>\n"
+html_content += "<canvas id='myChart'></canvas>\n"
+html_content += "</div>\n"
+html_content += "  <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>\n"
+html_content += "<script>\n"
+html_content += "var ctx = document.getElementById('myChart').getContext('2d');\n"
+html_content += "var myChart = new Chart(ctx, {\n"
+html_content += "  type: 'line',\n"
+html_content += "  data: {\n"
+html_content += "    datasets: [\n"
+html_content += chart_data + "\n"
+html_content += "    ]\n"
+html_content += "  },\n"
+html_content += "  options: {\n"
+html_content += "    scales: {\n"
+html_content += "      xAxes: [{\n"
+html_content += "        type: 'time',\n"
+html_content += "        time: {\n"
+html_content += "           unit: 'month',\n"
+html_content += "           displayFormats: {\n"
+html_content += "            month: 'YYYY-MM'\n"
+html_content += "           }\n"
+html_content += "        }\n"
+html_content += "      }],\n"
+html_content += "      yAxes: [{\n"
+html_content += "        ticks: {\n"
+html_content += "          beginAtZero: true\n"
+html_content += "        }\n"
+html_content += "      }]\n"
+html_content += "    }\n"
+html_content += "  }\n"
+html_content += "});\n"
+html_content += "</script>\n"
+html_content += "  <p>Aggregated kewords tweeted stats by month</p>\n"
+
+
+html_content += "</head>\n"
+html_content += "<body>\n"
+
+#, Crea un dizionario vuoto per gli utenti
+users_dict = {}
+
+# Utilizza un ciclo for per aggiungere gli utenti al dizionario e tenere traccia della loro posizione nell'HTML
+pos = 0
+for _, row in df_podcast.iterrows():
+    user = row["Profile"]
+    if user not in users_dict:
+        users_dict[user] = pos
+        pos += 1
+
+# Utilizza un ciclo for per creare un elenco di link agli utenti nell'HTML
+html_content += "  <h3>List of Authors</h3>\n"
+html_content += "  <ul>\n"
+for user in users_dict:
+    html_content += f"    <li><a href='#user{users_dict[user]}'>{user}</a></li>\n"
+html_content += "  </ul>\n"
+
+# Utilizza un ciclo for per iterare attraverso ogni riga del dataframe
+current_user = df_podcast.iloc[0]["Profile"]
+html_content += f"  <h2 class='h2' style='text-transform: uppercase;' id='user{users_dict[current_user]}'>{current_user} </h2><br><a href='{row['Profile_web']}'>Official website</a><br><a href='{row['Profile_twi']}'>Twitter profile</a>\n"
+
+for _, row in df_podcast.iterrows():
+    user = row["Profile"]
+    if user != current_user:
+        current_user = user
+        
+# Crea un'ancora per ogni utente nell'HTML
+        html_content += f"  <h2 class='h2' style='text-transform: uppercase;' id='user{users_dict[user]}'>{user}</h2><br><a href='{row['Profile_web']}'>Official website</a><br><a href='{row['Profile_twi']}'>Twitter profile</a>\n"
+    date = row["Time"]
+    text = make_link(row["Tweet"])
+    html_content += f" <h3 date='{date}'>{date}</h3>\n"
+    html_content += f"  <p>{text}</p>\n"
+
+html_content += "</body>\n"
+html_content += "<footer>\n"
+html_content += "  <p>Made with ❤️ for the community by Riccardo</p>\n"
+html_content += "</footer>\n"
+html_content += "</html>\n"
+
+with open("./podcast.html", "w") as f:
+    f.write(html_content)
+###
 
 # write countapi data in csv file
 url = "https://api.countapi.xyz/get/"+ countapi_workspace +"/"+ countapiID +"/"
